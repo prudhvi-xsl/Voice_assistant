596df9b90ec0080dea1b15d74335f1f895af91ee
prudhvi sai <prudhvisai7379@gmail.com>
2024-12-22 20:40:48 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,84 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { RetellWebClient } from "retell-client-js-sdk";
 
-const webClient = new RetellWebClient()
+const retelAPI = new RetellWebClient()
 
 function App() {
-  const [isCalling, setIsCalling] = useState(false)
-  const [buttonVariant, setButtonVariant] = useState("success")
- 
-  const [transcript, setTranscript] = useState("")
+  const [isCallStarted, setIsCallStarted] = useState(false)
 
-  // Initialize the SDK
+
   useEffect(() => {
-    // Setup event listeners
-    webClient.on("conversationStarted", () => {
-      console.log("conversationStarted")
+    retelAPI.on("conversationEnded", convEnded => {
+      setIsCallStarted(false) 
     })
 
-    webClient.on("audio", audio => {
-      console.log("There is audio")
-    })
-
-    webClient.on("conversationEnded", convEnded => {
-      console.log(
-        "Closed with code:",
-        convEnded.code,
-        ", reason:",
-        convEnded.reason
-      )
-      setIsCalling(false) // Update button to "Start" when conversation ends
-    })
-
-    webClient.on("error", error => {
-      console.error("An error occurred:", error)
-      setIsCalling(false) // Update button to "Start" in case of error
-    })
-
-    webClient.on("update", update => {
-      update = update.transcript[update.transcript.length - 1]
-      console.log("update", update.content)
-      setTranscript(
-        update.role == "user"
-          ? "Customer: " + update.content
-          : "Assistant: " + update.content
-      )
+    retelAPI.on("error", error => {
+      setIsCallStarted(false) 
     })
   }, [])
 
-  const toggleConversation = async () => {
-    if (isCalling) {
-      webClient.stopCall()
-      setButtonVariant("success")
-      setIsCalling(false)
+  const onCliclStartStop =  () => {
+    if (isCallStarted) {
+      retelAPI.stopCall()
+      setIsCallStarted(false)
     } else {
-      const registerCallResponse = await registerCall()
-      if (registerCallResponse.call_id) {
-        await webClient.startCall({
-          accessToken: registerCallResponse.access_token,
-        });
-        setIsCalling(true) // Update button to "Stop" when conversation starts
-        setButtonVariant("danger")
-      }
+     getCallInformation()
     }
   }
 
-  async function registerCall() {
+ const getCallInformation=async()=> {
     try {
-      // Replace with your server url
       const response = await fetch("https://api.retellai.com/v2/create-web-call", {
-        method: 'POST', // Specify the method as POST
+        method: 'POST', 
         headers: {
-          'Authorization': 'Bearer key_b322466cd9d79928ddd88d10ed6a', // Replace with your actual Bearer token
-                    'Content-Type': 'application/json',
+          'Authorization': 'Bearer key_b322466cd9d79928ddd88d10ed6a',
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ agent_id :"agent_8e8d2a877b2399c2172d19a0dc"}) // Convert the data object to a JSON string
+        body: JSON.stringify({ agent_id :"agent_8e8d2a877b2399c2172d19a0dc"}) 
       })
-      console.log(response,"REP")
-      let callDetails = await response.json()
-      return callDetails
+     const callInformation = await response.json()
+     
+     if (callInformation?.access_token) {
+       await retelAPI.startCall({
+        accessToken: callInformation.access_token,
+      });
+      setIsCallStarted(true) 
+     }
+      
     } catch (err) {
-      console.log(err)
       throw new Error(err)
     }
   }
@@ -86,8 +53,7 @@ function App() {
 
   return(
     <div>
-      <button onClick={toggleConversation} >{isCalling  ? 'Stop':'Start'} </button>
-      <p>{transcript}</p>
+      <button onClick={onCliclStartStop} >{isCallStarted  ? 'Stop':'Start'} </button>
     </div>
   )
 }
